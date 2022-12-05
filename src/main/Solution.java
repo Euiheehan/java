9d1ddffbb9c56eb1a5140b7f76bf7ffe6f5baf2c
miniLove <han419120@naver.com>
2022-12-05 21:37:51 +0900

    A를 B로 만들기(정렬 비교)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,25 +10,12 @@ class Main {
     }
 
     public int solution(String before, String after) {
-        String[] beforeArr = before.split("");
-        String[] afterArr = after.split("");
+        char[] beforeArr = before.toCharArray();
+        char[] afterArr = after.toCharArray();
 
-        Map<String, Integer> map  = new HashMap<>();
-        for(String s1:afterArr){
-            map.put(s1, map.getOrDefault(s1, 0)+1);
-        }
+        Arrays.sort(beforeArr);
+        Arrays.sort(afterArr);
 
-        for (String s : beforeArr) {
-            if(map.get(s) == null) return 0;
-            if(map.get(s) == 1){
-                map.remove(s);
-            }else{
-                map.put(s, map.get(s)-1);
-            }
-        }
-        if(!map.isEmpty()){
-            return 0;
-        }
-        return 1;
+        return new String(beforeArr).equalsIgnoreCase(new String(afterArr))? 1: 0;
     }
 }
