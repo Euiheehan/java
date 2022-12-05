e494d0d512703615d31f7d47b5d1110193b1f8fc
miniLove <han419120@naver.com>
2022-12-05 21:35:30 +0900

    A를 B로 만들기(map)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,27 +1,33 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Main {
     public static void main(String[] args) {
-
-
-          System.out.print(new Main().solution(".... . .-.. .-.. ---"));
+          System.out.print(new Main().solution("olleh","hello"));
     }
 
-    public String solution(String letter) {
+    public int solution(String before, String after) {
+        String[] beforeArr = before.split("");
+        String[] afterArr = after.split("");
 
-        String[] arr = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        StringBuilder answer = new StringBuilder();
-        String[] letters = letter.split(" ");
-
-        for(String s : letters){
-            for(int i = 0; i<arr.length; i++){
-                if(s.equalsIgnoreCase(arr[i])){
-                    answer.append(Character.toChars(i+97));
-                    break;
-                }
-            }
+        Map<String, Integer> map  = new HashMap<>();
+        for(String s1:afterArr){
+            map.put(s1, map.getOrDefault(s1, 0)+1);
         }
 
-        return answer.toString();
+        for (String s : beforeArr) {
+            if(map.get(s) == null) return 0;
+            if(map.get(s) == 1){
+                map.remove(s);
+            }else{
+                map.put(s, map.get(s)-1);
+            }
+        }
+        if(!map.isEmpty()){
+            return 0;
+        }
+        return 1;
     }
 }
