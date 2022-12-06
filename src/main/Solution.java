fd5c526bbf20954557817e88c2423a04692f1eaf
miniLove <han419120@naver.com>
2022-12-06 21:31:44 +0900

    한번만 등장한 문자

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,21 +1,24 @@
 package main;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) {
-          System.out.print(new Main().solution("olleh","hello"));
+          System.out.print(new Main().solution("abdc"));
     }
 
-    public int solution(String before, String after) {
-        char[] beforeArr = before.toCharArray();
-        char[] afterArr = after.toCharArray();
+    public String solution(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i =0; i<s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+        }
 
-        Arrays.sort(beforeArr);
-        Arrays.sort(afterArr);
-
-        return new String(beforeArr).equalsIgnoreCase(new String(afterArr))? 1: 0;
+        return map.keySet().stream()
+                .filter(character -> map.get(character) == 1)
+                .map(String::valueOf)
+                .sorted()
+                .collect(Collectors.joining());
     }
 }
