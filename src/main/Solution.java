023629aeb08afe4f81202cecb6a43e8e30a44ee6
miniLove <han419120@naver.com>
2022-12-07 23:50:10 +0900

    외계어 사전

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,24 +1,21 @@
 package main;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
-          System.out.print(new Main().solution("abdc"));
+        System.out.println(new Main().solution(new String[]{"s", "o", "m", "d"}, new String[]{"moos", "dzx", "smm", "sunmmo", "som"}));
     }
 
-    public String solution(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i =0; i<s.length(); i++){
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+    public int solution(String[] spell, String[] dic) {
+        for(String s : dic){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            Arrays.sort(spell);
+            if(new String(arr).equals(String.join("",spell))){
+                return 1;
+            }
         }
-
-        return map.keySet().stream()
-                .filter(character -> map.get(character) == 1)
-                .map(String::valueOf)
-                .sorted()
-                .collect(Collectors.joining());
+        return 2;
     }
 }
