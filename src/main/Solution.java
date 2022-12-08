f258c6a0298b8aadf41c1711c28f1535e6b80d8e
miniLove <han419120@naver.com>
2022-12-08 18:09:30 +0900

    폰켓몬

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -4,30 +4,24 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        for(int n :new Main().solution(new int[]{1, 2, 3, 4, 5, 6},4)){
-            System.out.print(n+" ");
-        }
+        long start = System.currentTimeMillis();
+        System.out.println(new Main().solution(new int[]{3,3,4,5,5,2,2,2,1,5,6,6,5,4,3,4,34,234,23,423,4,3,3,3,4,3,6,7,8,8,5,45,4,4,4,6,7,7,7,9,3,2,2,4,3,3,4,5,5,2,2,2,1,5,6,6,5,4,3,4,34,234,23,423,4,3,3,3,4,3,6,7,8,8,5,45,4,4,4,6,7,7,7,9,3,2,2,4,3,3,4,5,5,2,2,2,1,5,6,6,5,4,3,4,34,234,23,423,4,3,3,3,4,3,6,7,8,8,5,45,4,4,4,6,7,7,7,9,3,2,2,4,3,3,4,5,5,2,2,2,1,5,6,6,5,4,3,4,34,234,23,423,4,3,3,3,4,3,6,7,8,8,5,45,4,4,4,6,7,7,7,9,3,2,2,4}));
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
     }
 
-    public int[] solution(int[] numlist, int n) {
-        int[] answer = new int[numlist.length];
-        Map<Integer, Integer> map = new HashMap<>();
+    public int solution(int[] nums) {
+        int pickUpCount = nums.length/2;
 
-        for(int num : numlist){
-            map.put(num, map.getOrDefault(num,Math.abs(n-num)));
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
-        List<Integer> keys = new ArrayList<>(map.keySet());
-        keys.sort((o1, o2) -> {
-            if(Objects.equals(map.get(o1), map.get(o2))){
-                return o2.compareTo(o1);
-            }
-            return map.get(o1).compareTo(map.get(o2));
-        });
 
+        //distinct보다 HashSet이 빠른이유는 distinct는 statful하기 때문
+        //int kindCount = (int)Arrays.stream(nums).distinct().count();
 
-        for (int i =0; i<keys.size(); i++){
-            answer[i] = keys.get(i);
-        }
-        return answer;
+        //return Math.min(pickUpCount, kindCount);
+        return Math.min(pickUpCount, set.size());
     }
 }
