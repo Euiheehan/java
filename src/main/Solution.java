05cdb2e87a088ee9dfb1839550c56bba2ac3461b
miniLove <han419120@naver.com>
2022-12-11 23:54:35 +0900

    봉우리 - 겉에 0으로 된 테두리 없이,테두리의 숫자는 예외 조건 걸어서 푸는 법

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -19,28 +19,37 @@ class Main {
 
     public int solution(int[][] arr) {
 
-        int count = 0;
+        int answer = 0;
+        //행
+        int[]dx = {-1,0,1,0};
+        //열
+        int[]dy = {0,1,0,-1};
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+        /*
+         * (1,1) 기준으로 4개를 비교해야될 때,
+         * [0,0] [1,1] [2,2] [3,3]
+         * (0,1) (1,2) (2,1) (1,0) 을 비교하는 것
+         * */
+
+
+        for (int i=0; i<arr.length; i++) {
+            for(int j = 0; j<arr.length; j++){
+                boolean isTrue = true;
                 int target = arr[i][j];
-
-                //겉 테두리에 0이 있다 생각하고
-                // 행이 target 기준으로 i-1,i,i+1
-                // 열이 target 기준으로 j-1,j,j+1
-
-                //i-1, j-1 0보다 작으면 0 임
-                //i+1, i+1 배열 최대보다 크면 0 임
-
-                if (target > (j - 1 < 0 ? 0 : arr[i][j - 1])
-                        && target > (j + 1 > arr.length - 1 ? 0 : arr[i][j + 1])
-                        && target > (i - 1 < 0 ? 0 : arr[i - 1][j])
-                        && target > (i + 1 > arr.length - 1 ? 0 : arr[i + 1][j])) {
-                    count++;
+                for(int k = 0; k<4; k++){
+                    int nx = i+dx[k];
+                    int ny = j+dy[k];
+                    if(nx >= 0 && nx <arr.length
+                            && ny >= 0 && ny <arr.length
+                            && target <= arr[nx][ny]) {
+                        isTrue=false;
+                        break;
+                    }
                 }
+                answer += isTrue? 1 : 0;
             }
         }
-        return count;
+        return answer;
     }
 }
 
