f778e630a8c417fe8e9486a6b9daa420cc00e0df
miniLove <han419120@naver.com>
2022-12-13 18:56:37 +0900

    이진수 더하기 - 함수 사용 → 속도, 가독성 향상 버전

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -7,38 +7,7 @@ class Main {
         System.out.println(new Main().solution("100001", "1010101"));
     }
     public String solution(String bin1, String bin2) {
-        StringBuilder answer = new StringBuilder();
-        int len;
-        if(bin1.length() < bin2.length()){
-            len = bin2.length();
-            bin1 = "0".repeat(bin2.length()-bin1.length())+bin1;
-        }else if(bin1.length() > bin2.length()){
-            len = bin1.length();
-            bin2 = "0".repeat(bin1.length()-bin2.length())+bin2;
-        }else {
-            len = bin1.length();
-        }
-
-        int addSum = 0;
-        for(int i = len-1; i >= 0; i--){
-            int sum = Character.getNumericValue(bin1.charAt(i)) + Character.getNumericValue(bin2.charAt(i)); // 더한 값
-            int total10Sum = sum+addSum;
-
-            if(total10Sum == 3){
-                answer.append("1");
-            } else if (total10Sum == 2) {
-                answer.append("0");
-            }else{
-                answer.append(total10Sum);
-            }
-
-            addSum = total10Sum > 1? 1 : 0;
-        }
-
-        if(addSum == 1){
-            answer.append("1");
-        }
-        return answer.reverse().toString();
+        return Integer.toBinaryString(Integer.parseInt(bin1,2)+Integer.parseInt(bin2,2));
     }
 }
 
