055d349f4ca141be91b3c06de770072b839600ad
miniLove <han419120@naver.com>
2022-12-14 20:14:28 +0900

    2차원으로 풀기 - 반복문 줄이기

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -9,13 +9,8 @@ class Main {
     public int[][] solution(int[] num_list, int n) {
         int[][] answer = new int[num_list.length/n][n];
 
-        int idx = 0;
-
-        for(int i = 0; i<num_list.length/n; i++){
-            for(int j =0; j<n; j++){
-                answer[i][j] = num_list[idx];
-                idx++;
-            }
+        for(int i=0; i<num_list.length; i++){
+            answer[i/n][i%n]=num_list[i];
         }
         return answer;
     }
