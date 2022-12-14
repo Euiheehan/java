d687741955b9dac7f24d71b1ce191176c2c06788
miniLove <han419120@naver.com>
2022-12-14 20:13:54 +0900

    2차원으로 풀기

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,39 +1,23 @@
 package main;
 
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println(new Main().solution(new int[][]{{1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}}));
+        System.out.println(Arrays.deepToString(new Main().solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 2)));
     }
-    public int solution(int[][] board) {
-        //위험지역 check x,y 좌표
-        int [] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int [] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length/n][n];
 
-        int warningCount = 0;
-        int len = board.length;
+        int idx = 0;
 
-        for(int i = 0; i<len; i++){
-            for(int j = 0; j<len; j++){
-                if(board[i][j] == 1){
-                    //실제 위험지역 ++
-                   warningCount++;
-
-                   for(int k = 0; k<dx.length; k++){
-                       //배열 범위인지 check, 위험지역 체크한거 다시 체크하지 않기
-                       if( (i+dx[k] >-1 && i+dx[k] < len)
-                           && (j+dy[k] >-1 && j+dy[k] <len)
-                           && board[i+dx[k]][j+dy[k]] == 0
-                       ){
-                           //파생 위험지역 (1로 넣어주면 실제 위험지역과 구분이 안되므로 다른숫자 넣어주기)
-                           warningCount++;
-                           board[i+dx[k]][j+dy[k]] = 2;
-
-                       }
-                   }
-                }
+        for(int i = 0; i<num_list.length/n; i++){
+            for(int j =0; j<n; j++){
+                answer[i][j] = num_list[idx];
+                idx++;
             }
         }
-        return len*len - warningCount;
+        return answer;
     }
 }
 
