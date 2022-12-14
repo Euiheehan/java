c8a012974b489bf28d5ec2c507de9c73e4298e98
miniLove <han419120@naver.com>
2022-12-14 19:46:53 +0900

    안전지대

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,13 +1,39 @@
 package main;
 
-import java.util.*;
-
 class Main {
     public static void main(String[] args) {
-        System.out.println(new Main().solution("100001", "1010101"));
+        System.out.println(new Main().solution(new int[][]{{1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}}));
     }
-    public String solution(String bin1, String bin2) {
-        return Integer.toBinaryString(Integer.parseInt(bin1,2)+Integer.parseInt(bin2,2));
+    public int solution(int[][] board) {
+        //위험지역 check x,y 좌표
+        int [] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int [] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        int warningCount = 0;
+        int len = board.length;
+
+        for(int i = 0; i<len; i++){
+            for(int j = 0; j<len; j++){
+                if(board[i][j] == 1){
+                    //실제 위험지역 ++
+                   warningCount++;
+
+                   for(int k = 0; k<dx.length; k++){
+                       //배열 범위인지 check, 위험지역 체크한거 다시 체크하지 않기
+                       if( (i+dx[k] >-1 && i+dx[k] < len)
+                           && (j+dy[k] >-1 && j+dy[k] <len)
+                           && board[i+dx[k]][j+dy[k]] == 0
+                       ){
+                           //파생 위험지역 (1로 넣어주면 실제 위험지역과 구분이 안되므로 다른숫자 넣어주기)
+                           warningCount++;
+                           board[i+dx[k]][j+dy[k]] = 2;
+
+                       }
+                   }
+                }
+            }
+        }
+        return len*len - warningCount;
     }
 }
 
