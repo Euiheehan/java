9e666ce539465137667110e827dc7aff382a8f76
miniLove <han419120@naver.com>
2022-12-16 01:56:57 +0900

    옹알이(1) - for문이 하나지만,이전 3중 for문 보다 느림. (3중 포문에는 break가 있기 때문에)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,24 +1,19 @@
 package main;
 
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println(new Main().solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa"}));
+        System.out.println(new Main().solution(new String[]{"ayaye", "uuuma", "ye", "wyeoo", "ayaa"}));
     }
 
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] bases = {"aya", "ye", "woo", "ma"};
 
-        for(String babb : babbling){
-            for(String base : bases){
-                //babb = babb.replaceFirst(base, "");
-                //이렇게 하게 되면 wyeoo -> ye가 치환되고 woo가 남아 치환되어 잘못된 count가 올라간다.
-                babb = babb.replaceFirst(base, "#");
-                //if(babb.replaceAll("#","").equals("")){  -- replace로 바꾸면 최대 5초 줄어들기 때문에
-                if(babb.replace("#","").equals("")){
-                    answer++;
-                    break;
-                }
+        for(int i =0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replaceAll("aya|woo|ye|ma", "");
+            if(babbling[i].isEmpty()) {
+                answer++;
             }
         }
         return answer;
