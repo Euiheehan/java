4a9b913a4a9525feac54df23e77266a1ab92d78d
miniLove <han419120@naver.com>
2022-12-16 00:41:23 +0900

    옹알이(1)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,28 +1,43 @@
 package main;
 
-import java.math.BigInteger;
-
 class Main {
     public static void main(String[] args) {
-        System.out.println(new Main().solution(5,3));
+        System.out.println(new Main().solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa"}));
     }
 
-    public BigInteger solution(int balls, int share) {
+    public int solution(String[] babbling) {
+        String[] bases = {"aya", "ye", "woo", "ma"};
+        int answer = 0;
 
-        //공식은 balls! / (balls-share)! * share!
+        for(String s : babbling){
 
-        BigInteger de = BigInteger.ONE;
-        BigInteger nu = BigInteger.ONE;
+            boolean isContains = true;
+            int notCount = 0;
 
-        //ex)분모는 5! 이 아니라 5*4만 돌면 분자에서 3!안해줘도 되기 때문에.
-        for(int i = balls; i>share; i--){
-            de = de.multiply(BigInteger.valueOf(i));
+            //문자열이 없어질 때까지 반복
+            while(s.length()>0){
+                //해당 문자열의 접두어가 base중에 있는지 확인
+                for(String str : bases){
+                    if(s.startsWith(str)){
+                        //접두어에 있으면 그 문자열 비우고 다시 넣기
+                        s = s.replaceAll(str,"");
+                        //이거 안해주면 notCount는 누적이라 yemawoo같은 경우는 밑에서 false로 걸림
+                        notCount = 0;
+                        break;
+                    }else{
+                        //맞는것이 없으면 ++
+                        notCount++;
+                    }
+                }
+                //다 돌았는데도 notCount가 base길이랑 같으면 하나도 안맞는거니까 다음 문자열로.
+                if(notCount == bases.length) {
+                    isContains = false;
+                    break;
+                }
+            }
+            if(isContains) answer++;
         }
-
-        for(int i = 1; i<=(balls-share); i++){
-            nu  = nu.multiply(BigInteger.valueOf(i));
-        }
-        return de.divide(nu);
+        return answer;
     }
 }
 
