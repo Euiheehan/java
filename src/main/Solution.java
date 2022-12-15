fd83b59c38a8219ddefb7d74e70b3ed2ec19d1f7
miniLove <han419120@naver.com>
2022-12-16 01:41:45 +0900

    옹알이(1) - for depth는 하나 줄였지만, 속도가 이전 3중 for문 보다 느림.

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -6,36 +6,20 @@ class Main {
     }
 
     public int solution(String[] babbling) {
-        String[] bases = {"aya", "ye", "woo", "ma"};
         int answer = 0;
+        String[] bases = {"aya", "ye", "woo", "ma"};
 
-        for(String s : babbling){
-
-            boolean isContains = true;
-            int notCount = 0;
-
-            //문자열이 없어질 때까지 반복
-            while(s.length()>0){
-                //해당 문자열의 접두어가 base중에 있는지 확인
-                for(String str : bases){
-                    if(s.startsWith(str)){
-                        //접두어에 있으면 그 문자열 비우고 다시 넣기
-                        s = s.replaceAll(str,"");
-                        //이거 안해주면 notCount는 누적이라 yemawoo같은 경우는 밑에서 false로 걸림
-                        notCount = 0;
-                        break;
-                    }else{
-                        //맞는것이 없으면 ++
-                        notCount++;
-                    }
-                }
-                //다 돌았는데도 notCount가 base길이랑 같으면 하나도 안맞는거니까 다음 문자열로.
-                if(notCount == bases.length) {
-                    isContains = false;
+        for(String babb : babbling){
+            for(String base : bases){
+                //babb = babb.replaceFirst(base, "");
+                //이렇게 하게 되면 wyeoo -> ye가 치환되고 woo가 남아 치환되어 잘못된 count가 올라간다.
+                babb = babb.replaceFirst(base, "#");
+                //if(babb.replaceAll("#","").equals("")){  -- replace로 바꾸면 최대 5초 줄어들기 때문에
+                if(babb.replace("#","").equals("")){
+                    answer++;
                     break;
                 }
             }
-            if(isContains) answer++;
         }
         return answer;
     }
