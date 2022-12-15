a671a7e5ed2ac36d05b5fd1459cd5c114cc57d18
miniLove <han419120@naver.com>
2022-12-15 22:40:14 +0900

    구슬을 나누는 경우의 수

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,18 +1,28 @@
 package main;
 
-import java.util.Arrays;
+import java.math.BigInteger;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(new Main().solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 2)));
+        System.out.println(new Main().solution(5,3));
     }
-    public int[][] solution(int[] num_list, int n) {
-        int[][] answer = new int[num_list.length/n][n];
 
-        for(int i=0; i<num_list.length; i++){
-            answer[i/n][i%n]=num_list[i];
+    public BigInteger solution(int balls, int share) {
+
+        //공식은 balls! / (balls-share)! * share!
+
+        BigInteger de = BigInteger.ONE;
+        BigInteger nu = BigInteger.ONE;
+
+        //ex)분모는 5! 이 아니라 5*4만 돌면 분자에서 3!안해줘도 되기 때문에.
+        for(int i = balls; i>share; i--){
+            de = de.multiply(BigInteger.valueOf(i));
         }
-        return answer;
+
+        for(int i = 1; i<=(balls-share); i++){
+            nu  = nu.multiply(BigInteger.valueOf(i));
+        }
+        return de.divide(nu);
     }
 }
 
