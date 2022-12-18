058ba9dd25e559e27dd62ee32bf58ad2da72a5a9
miniLove <han419120@naver.com>
2022-12-18 21:44:14 +0900

    같은 숫자는 싫어

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,20 +1,27 @@
 package main;
 
+
+import java.util.*;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println(new Main().solution(new String[]{"ayaye", "uuuma", "ye", "wyeoo", "ayaa"}));
+        System.out.println(Arrays.toString(new Main().solution(new int[]{1, 1, 3, 3, 0, 1, 1})));
     }
 
-    public int solution(String[] babbling) {
-        int answer = 0;
-        for (String s : babbling) {
-            //if (s.matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")) {
-            //이것을 좀 더 쉽게한 것이 아래꺼
-            if (s.matches("(aya|ye|woo|ma)+")) {
-                answer++;
-            }
+    public int[] solution(int []arr) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(arr[0]);
+
+        List<Integer> list = new ArrayList<>();
+        for(int i : arr){
+           if(i != queue.element()){
+               queue.offer(i);
+               list.add(queue.poll());
+           }
         }
-        return answer;
+        list.add(queue.poll());
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
 
