545afce66cc7f8aa23087e91930cd5b3ee665c33
miniLove <han419120@naver.com>
2022-12-19 21:05:35 +0900

    약수의 개수와 덧셈

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,27 +1,35 @@
 package main;
 
-
-import java.util.*;
-
 class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Main().solution(new int[]{1, 1, 3, 3, 0, 1, 1})));
+        System.out.println(new Main().solution(13,17));
     }
 
-    public int[] solution(int []arr) {
+    public int solution(int left, int right) {
+        int answer = 0;
 
-        List<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
-        for(int i : arr){
-           if(list.get(list.size()-1) != i){
-               list.add(i);
-           }
+        for(int i =left; i<=right; i++){
+            answer = divisorCount(i)%2 ==0? answer+i : answer-i;
         }
-        int[] result = new int[list.size()];
-        for(int i = 0; i< result.length; i++){
-            result[i] = list.get(i);
+        return answer;
+    }
+
+    private int divisorCount(int num){
+        if(num == 1) return 1;
+        int count = 0;
+        //num이 20이면 4*4까지만 돌아도된다.
+        for(int i =1; i*i<=num; i++){
+            //그 중 나누어 떨어지는 수
+            if(num % i ==0){
+                //몫이 나누는수와 같으면 ex) 4*4
+                if(num/i == i){
+                    count++;
+                }else{
+                    count +=2;
+                }
+            }
         }
-        return result;
+        return count;
     }
 }
 
