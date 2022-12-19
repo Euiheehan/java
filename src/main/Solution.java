af9105143dbd0273d120015d4a33e8f7a6dbcc2f
miniLove <han419120@naver.com>
2022-12-19 21:11:00 +0900

    약수의 개수와 덧셈 - 함수써서 속도 향상과 코드 가독성 업 버전

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -9,27 +9,11 @@ class Main {
         int answer = 0;
 
         for(int i =left; i<=right; i++){
-            answer = divisorCount(i)%2 ==0? answer+i : answer-i;
+            //제곱수인 경우 약수의 개수는 홀수,
+            //제곱수가 아닌 약수의 개수는 짝수
+            answer = i%Math.sqrt(i) == 0? answer-i : answer+i;
         }
         return answer;
-    }
-
-    private int divisorCount(int num){
-        if(num == 1) return 1;
-        int count = 0;
-        //num이 20이면 4*4까지만 돌아도된다.
-        for(int i =1; i*i<=num; i++){
-            //그 중 나누어 떨어지는 수
-            if(num % i ==0){
-                //몫이 나누는수와 같으면 ex) 4*4
-                if(num/i == i){
-                    count++;
-                }else{
-                    count +=2;
-                }
-            }
-        }
-        return count;
     }
 }
 
