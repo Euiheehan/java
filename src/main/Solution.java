d571e36d0fa39d55ca5214e6821eda7dae36f4c3
miniLove <han419120@naver.com>
2022-12-20 21:13:39 +0900

    3진법 뒤집기

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -2,18 +2,13 @@ package main;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println(new Main().solution(13,17));
+        System.out.println(new Main().solution(45));
     }
 
-    public int solution(int left, int right) {
-        int answer = 0;
+    public int solution(int n) {
+        String num = Integer.toString(n,3);
+        return Integer.parseInt(new StringBuilder(num).reverse().toString(),3);
 
-        for(int i =left; i<=right; i++){
-            //제곱수인 경우 약수의 개수는 홀수,
-            //제곱수가 아닌 약수의 개수는 짝수
-            answer = i%Math.sqrt(i) == 0? answer-i : answer+i;
-        }
-        return answer;
     }
 }
 
