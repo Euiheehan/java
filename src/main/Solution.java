66037e8262d7745be12baf820c0237e27cc5b5df
miniLove <han419120@naver.com>
2022-12-20 21:46:45 +0900

    문자열 내 마음대로 정렬하기

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,14 +1,24 @@
 package main;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println(new Main().solution(45));
+        System.out.println(Arrays.toString(new Main().solution(new String[]{"sun", "bed", "car"}, 1)));
     }
 
-    public int solution(int n) {
-        String num = Integer.toString(n,3);
-        return Integer.parseInt(new StringBuilder(num).reverse().toString(),3);
-
+    public String[] solution(String[] strings, int n) {
+        return Arrays.stream(strings)
+                .sorted((o1, o2) -> {
+                    if(o1.charAt(n) == o2.charAt(n)){
+                        return o1.compareTo(o2);
+                    }else{
+                        return String.valueOf(o1.charAt(n)).compareTo(String.valueOf(o2.charAt(n)));
+                    }
+                })
+                .toArray(String[]::new);
     }
 }
 
