c5d15abdbf910d408274db966c7eba26fca4116d
miniLove <han419120@naver.com>
2022-12-21 23:44:48 +0900

    k번째수

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -4,15 +4,24 @@ import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Main().solution(new String[]{"sun", "bed", "car"}, 1)));
+        System.out.println(Arrays.toString(new Main().solution(
+                new int[]{1, 5, 2, 6, 3, 7, 4}
+                , new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}})));
     }
 
-    public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, (s1, s2) -> {
-            if(s1.charAt(n) == s2.charAt(n)) return s1.compareTo(s2);
-            else return s1.charAt(n)-s2.charAt(n);
-        });
-        return strings;
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i =0; i<commands.length; i++){
+            int subStart = commands[i][0]-1;
+            int subEnd = commands[i][1];
+            int k = commands[i][2]-1;
+
+            int[] subStringArr = Arrays.stream(Arrays.copyOfRange(array, subStart, subEnd)).sorted().toArray();
+            answer[i] = subStringArr[k];
+        }
+
+        return answer;
     }
 }
 
