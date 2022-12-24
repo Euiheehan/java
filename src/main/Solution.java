6f8bebe78be303b2e7dd5ee843f9a4691dd04892
miniLove <han419120@naver.com>
2022-12-24 23:16:15 +0900

    4621 : 공 꺼내기

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,48 +1,62 @@
 package main;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Main {
-    static int n = 0;
-    static int m = 0;
-    static int answer = 0;
-
-    static int[][] graph;
-    static int[] ch;
-
     public static void main(String[] args) {
-        Main main = new Main();
-        Scanner sc = new Scanner(System.in);
+        Main main =  new Main();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-        n = sc.nextInt();
-        m = sc.nextInt();
-        graph = new int[n+1][n+1];
-        ch = new int[n+1];
+        int[] ball = new int[n];
+        int[] order = new int[n];
 
-        for(int i = 0; i<m; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            graph[a][b] = 1;
+        for(int i =0; i<n; i++){
+            ball[i] = scanner.nextInt();
         }
-        //출발노드 check
-        ch[1] = 1;
-        main.DFS(1);
-        System.out.println(answer);
+
+        for(int i =0; i<n; i++){
+            order[i] = scanner.nextInt();
+        }
+
+        System.out.println(Arrays.toString(main.solution(ball, order)));
+
     }
 
-    public void DFS(int v) {
-        if( v == n ){
-            answer++;
-        }else{
-            for(int i=1; i<=n; i++){
-                if(graph[v][i] == 1 && ch[i] == 0){
-                    //체크 넣어주기
-                    ch[i] =1;
-                    DFS(i);
-                    //back한 경우는 체크 풀어주기
-                    ch[i] = 0;
+    public int[] solution(int[] ball, int[] order) {
+
+        List<Integer> ballList = Arrays.stream(ball).boxed().collect(Collectors.toCollection(LinkedList::new));
+        List<Integer> orderList = Arrays.stream(order).boxed().collect(Collectors.toCollection(ArrayList::new));
+        List<Integer> tempList = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        int orderIndex = 0;
+        while(!ballList.isEmpty()){
+
+            if(!tempList.isEmpty()){
+                int tempIndex = 0;
+                while(tempIndex != tempList.size()){
+                    if(ballList.get(0).equals(tempList.get(tempIndex))
+                            || ballList.get(ballList.size()-1).equals(tempList.get(tempIndex))){
+                        ballList.remove(ballList.get(0).equals(tempList.get(tempIndex))? 0 : ballList.size() - 1);
+                        result.add(tempList.get(tempIndex));
+                        tempList.remove(tempIndex);
+                        tempIndex = 0;
+                    }else{
+                        tempIndex++;
+                    }
                 }
             }
+
+            if(ballList.get(0).equals(orderList.get(orderIndex)) || ballList.get(ballList.size()-1).equals(orderList.get(orderIndex))){
+                ballList.remove(ballList.get(0).equals(orderList.get(orderIndex))? 0 : ballList.size()-1);
+                result.add(orderList.get(orderIndex));
+            }else{
+                tempList.add(orderList.get(orderIndex));
+            }
+            orderIndex++;
         }
+       return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
