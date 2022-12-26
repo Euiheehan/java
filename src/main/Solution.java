2cd99cb378c0e20ffd9b63b1fbb9081382b37469
miniLove <han419120@naver.com>
2022-12-26 23:15:29 +0900

    최소 직사각형

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,62 +1,37 @@
 package main;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Main main =  new Main();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        int m = scanner.nextInt();
 
-        int[] ball = new int[n];
-        int[] order = new int[n];
-
+        int[][] sizes = new int[n][m];
         for(int i =0; i<n; i++){
-            ball[i] = scanner.nextInt();
+            for(int j =0; j<m; j++){
+                sizes[i][j] = scanner.nextInt();
+            }
         }
-
-        for(int i =0; i<n; i++){
-            order[i] = scanner.nextInt();
-        }
-
-        System.out.println(Arrays.toString(main.solution(ball, order)));
-
+        System.out.println(main.solution(sizes));
     }
 
-    public int[] solution(int[] ball, int[] order) {
+    public int solution(int[][] sizes) {
+        int maxX = 0;
+        int maxY = 0;
 
-        List<Integer> ballList = Arrays.stream(ball).boxed().collect(Collectors.toCollection(LinkedList::new));
-        List<Integer> orderList = Arrays.stream(order).boxed().collect(Collectors.toCollection(ArrayList::new));
-        List<Integer> tempList = new LinkedList<>();
-        List<Integer> result = new ArrayList<>();
-
-        int orderIndex = 0;
-        while(!ballList.isEmpty()){
-
-            if(!tempList.isEmpty()){
-                int tempIndex = 0;
-                while(tempIndex != tempList.size()){
-                    if(ballList.get(0).equals(tempList.get(tempIndex))
-                            || ballList.get(ballList.size()-1).equals(tempList.get(tempIndex))){
-                        ballList.remove(ballList.get(0).equals(tempList.get(tempIndex))? 0 : ballList.size() - 1);
-                        result.add(tempList.get(tempIndex));
-                        tempList.remove(tempIndex);
-                        tempIndex = 0;
-                    }else{
-                        tempIndex++;
-                    }
-                }
+        for(int i = 0; i<sizes.length; i++){
+            if(sizes[i][0]> sizes[i][1]){
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
             }
 
-            if(ballList.get(0).equals(orderList.get(orderIndex)) || ballList.get(ballList.size()-1).equals(orderList.get(orderIndex))){
-                ballList.remove(ballList.get(0).equals(orderList.get(orderIndex))? 0 : ballList.size()-1);
-                result.add(orderList.get(orderIndex));
-            }else{
-                tempList.add(orderList.get(orderIndex));
-            }
-            orderIndex++;
+            if(maxX < sizes[i][0]) maxX = sizes[i][0];
+            if(maxY < sizes[i][1]) maxY = sizes[i][1];
         }
-       return result.stream().mapToInt(Integer::intValue).toArray();
+        return maxX*maxY;
     }
 }
