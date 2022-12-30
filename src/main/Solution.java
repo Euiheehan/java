2edaaf0c34c616cfb8f50b5656effe6d603f4540
miniLove <han419120@naver.com>
2022-12-30 23:52:46 +0900

    모의고사

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -3,31 +3,49 @@ package main;
 import java.util.*;
 
 class Main {
+
     public static void main(String[] args) {
         Main main = new Main();
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
 
-        int[] arr = new int[n];
-
-        for(int i =0; i<n; i++){
-            arr[i] = scanner.nextInt();
-        }
-        System.out.println(main.solution(arr));
+        System.out.println(Arrays.toString(main.solution(new int[]{1, 3, 2, 4, 2})));
     }
 
-    public int solution(int[] number) {
-        int answer = 0;
+    public int[] solution(int[] answers) {
 
-        for(int i = 0; i<number.length; i++){
-            for(int j = i+1; j<number.length; j++){
-                for(int k = j+1; k<number.length; k++){
-                   if(number[i]+number[j]+number[k] == 0){
-                       answer++;
-                   }
-                }
-            }
+        List<Integer> answer = new ArrayList<>();
+
+        int[] a1 = {1, 2, 3, 4, 5};
+        int[] a2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] a3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> list3 = new ArrayList<>();
+
+
+        for(int i = 0; i< answers.length; i++ ){
+            list1.add(a1[i%a1.length]);
+            list2.add(a2[i%a2.length]);
+            list3.add(a3[i%a3.length]);
         }
-        return answer;
+
+        int ac = 0, bc = 0, cc = 0;
+        for(int i = 0; i< answers.length; i++){
+            if(answers[i]==list1.get(i)) ac++;
+            if(answers[i]==list2.get(i)) bc++;
+            if(answers[i]==list3.get(i)) cc++;
+        }
+
+        if(ac >= bc && ac >= cc){
+            answer.add(1);
+        }
+        if(bc >= cc && bc>= ac){
+            answer.add(2);
+        }
+        if(cc >= ac && cc>= bc){
+            answer.add(3);
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
