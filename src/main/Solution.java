e5b9785b87a19a31f7402007c8dff7ba9d29f3ad
miniLove <han419120@naver.com>
2023-01-01 20:22:21 +0900

    바둑이 승차(DFS)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -3,36 +3,42 @@ package main;
 import java.util.*;
 
 class Main {
+    static int max;
+    static int maxValue = 0;
 
     public static void main(String[] args) {
         Main main = new Main();
 
-        System.out.println(Arrays.toString(main.solution(new int[]{1, 3, 2, 4, 2})));
+        Scanner scanner = new Scanner(System.in);
+        int n =  scanner.nextInt();
+        int m =  scanner.nextInt();
+
+        int[] arr = new int[m];
+
+        for(int i = 0; i<m; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(main.solution(n, arr));
     }
 
-    public int[] solution(int[] answers) {
+    public int solution(int weight, int[] arr) {
+        max = weight;
+        DFS(0,0, arr);
+        return maxValue;
+    }
 
-        List<Integer> answerPerson = new ArrayList<>();
+    private void DFS(int index, int sum, int[] arr){
+        //sum이 max보다 크면 재귀돌면 X
+        if(sum > max) return;
 
-        int[] a1 = {1, 2, 3, 4, 5};
-        int[] a2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] a3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
-        int ac = 0, bc = 0, cc = 0;
-        for(int i = 0; i< answers.length; i++){
-            if(answers[i]==a1[i%a1.length]) ac++;
-            if(answers[i]==a2[i%a2.length]) bc++;
-            if(answers[i]==a3[i%a3.length]) cc++;
+        //sum이 max보다 작으면서 하나의 부분집합이 만들어졌을 때,
+        if(index == arr.length){
+            maxValue = Math.max(sum, maxValue);
+        }else{
+            //다음 배열의 원소를 사용O
+            DFS(index+1, sum+arr[index], arr);
+            //다음 배열의 원소를 사용X
+            DFS(index+1, sum, arr);
         }
-
-        if(ac >= bc && ac >= cc) answerPerson.add(1);
-        if(bc >= cc && bc>= ac) answerPerson.add(2);
-        if(cc >= ac && cc>= bc) answerPerson.add(3);
-
-        int[] result = new int[answerPerson.size()];
-        for(int i = 0; i<result.length; i++){
-            result[i] = answerPerson.get(i);
-        }
-        return result;
     }
 }
