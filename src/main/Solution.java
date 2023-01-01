61ab4b0cd1ecfa93d31ad7284ac393858bb2c7f4
miniLove <han419120@naver.com>
2023-01-02 00:59:25 +0900

    조합수(메모이제이션)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -3,44 +3,21 @@ package main;
 import java.util.*;
 
 class Main {
-
+    static int[][] dy;
     public static void main(String[] args) {
         Main main = new Main();
 
         Scanner scanner = new Scanner(System.in);
         int n =  scanner.nextInt();
-        int m =  scanner.nextInt();
-
-        int[] arr = new int[n];
-
-        for(int i=0; i<n; i++ ){
-            arr[i] = scanner.nextInt();
-        }
-        main.solution(n, m, arr);
+        int r =  scanner.nextInt();
+        dy = new int[n+1][n+1];
+        System.out.println(main.DFS(n,r));
     }
 
-    public void solution(int n, int m, int[] arr) {
+    public int DFS(int n, int r) {
 
-        Queue<Integer> q = new LinkedList<>();
-
-        q.offer(arr[0]);
-        int level = 0;
-        while(!q.isEmpty() && level<m){
-            int len = q.size();
-            for(int i = 0; i<len; i++){
-
-                Integer cur = q.poll();
-                if(cur == null) throw new NullPointerException();
-
-                for(int j =0; j<n; j++){
-                    if(cur !=arr[j])  {
-                        q.offer(arr[j]);
-                        System.out.println(cur+" "+arr[j]);
-                    }
-
-                }
-            }
-            level++;
-        }
+        if(dy[n][r] > 0) return dy[n][r];
+        if(n == r || r==0) return 1;
+        else return dy[n][r] = DFS(n-1, r-1)+DFS(n-1, r);
     }
 }
