16058c106a7827b078fc80e3465f6b7103893316
miniLove <han419120@naver.com>
2023-01-01 22:51:04 +0900

    중복순열 (BFS)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -3,47 +3,35 @@ package main;
 import java.util.*;
 
 class Main {
-    static int max;
-    static int minValue = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         Main main = new Main();
 
         Scanner scanner = new Scanner(System.in);
         int n =  scanner.nextInt();
-
-        int[]arr = new int[n];
-
-        for(int i = 0; i<n; i++){
-            arr[i] = scanner.nextInt();
-        }
-
-        int amount =  scanner.nextInt();
-        System.out.println(main.solution(amount, arr));
+        int m =  scanner.nextInt();
+        main.solution(n, m);
     }
 
-    public int solution(int amount, int[] arr) {
-        max = amount;
+    public void solution(int n, int m) {
 
-        Integer[] arrI = Arrays.stream(arr).boxed().toArray(Integer[]::new);
-        Arrays.sort(arrI, Collections.reverseOrder());
-        DFS(0,0, arrI);
-        return minValue;
-    }
+        Queue<Integer> q = new LinkedList<>();
 
-    private void DFS(int index, int sum, Integer[] arr){
-        //sum이 max보다 크면 재귀돌면 X
-        if(sum > max) return;
+        q.offer(1);
+        int level = 0;
+        while(!q.isEmpty() && level<m){
+            int len = q.size();
+            for(int i = 0; i<len; i++){
 
-        //더 도는 것을 방지
-        if(index >= minValue) return;
-        if(sum == max){
-            minValue = Math.min(sum, index);
-        }else{
-            for (int j : arr) {
-                //다음 배열의 원소를 사용O
-                DFS(index + 1, sum + j, arr);
+                Integer cur = q.poll();
+                if(cur == null) throw new NullPointerException();
+
+                for(int j =1; j<n+1; j++){
+                    q.offer(j);
+                    System.out.println(cur+" "+j);
+                }
             }
+            level++;
         }
     }
 }
