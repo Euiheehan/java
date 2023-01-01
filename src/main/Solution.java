2f5ddf258c9010c953aa29ce1b0cd723402f5a85
miniLove <han419120@naver.com>
2023-01-01 23:29:11 +0900

    순열구하기(DFS)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -7,25 +7,36 @@ class Main {
     static int n;
     static int m;
     static int[] pm;
+    static int[] ch;
+    static int[] result;
     public static void main(String[] args) {
         Main main = new Main();
 
         Scanner scanner = new Scanner(System.in);
         n =  scanner.nextInt();
         m =  scanner.nextInt();
-        pm = new int[m];
+        pm = new int[n];
+        ch = new int[n];
+        result = new int[m];
+        for(int i = 0; i<n; i++){
+            pm[i] =  scanner.nextInt();
+        }
         main.DFS(0);
     }
 
     public void DFS(int index) {
-        
+
         if(index == m){
-            for(int x : pm) System.out.print(x +" ");
+            for(int x : result) System.out.print(x +" ");
             System.out.println();
         }else{
-            for(int i =1; i<=n; i++){
-                pm[index] = i;
-                DFS(index+1);
+            for(int i =0; i<n; i++){
+                if(ch[i] == 0){
+                    ch[i] =1;
+                    result[index] = pm[i];
+                    DFS(index + 1);
+                    ch[i] =0;
+                }
             }
         }
     }
