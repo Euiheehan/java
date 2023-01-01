bc472e140eac91c68aa773362ae5208473078164
miniLove <han419120@naver.com>
2023-01-01 23:06:26 +0900

    중복순열 (DFS)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -4,34 +4,29 @@ import java.util.*;
 
 class Main {
 
+    static int n;
+    static int m;
+    static int[] pm;
     public static void main(String[] args) {
         Main main = new Main();
 
         Scanner scanner = new Scanner(System.in);
-        int n =  scanner.nextInt();
-        int m =  scanner.nextInt();
-        main.solution(n, m);
+        n =  scanner.nextInt();
+        m =  scanner.nextInt();
+        pm = new int[m];
+        main.DFS(0);
     }
 
-    public void solution(int n, int m) {
-
-        Queue<Integer> q = new LinkedList<>();
-
-        q.offer(1);
-        int level = 0;
-        while(!q.isEmpty() && level<m){
-            int len = q.size();
-            for(int i = 0; i<len; i++){
-
-                Integer cur = q.poll();
-                if(cur == null) throw new NullPointerException();
-
-                for(int j =1; j<n+1; j++){
-                    q.offer(j);
-                    System.out.println(cur+" "+j);
-                }
+    public void DFS(int index) {
+        
+        if(index == m){
+            for(int x : pm) System.out.print(x +" ");
+            System.out.println();
+        }else{
+            for(int i =1; i<=n; i++){
+                pm[index] = i;
+                DFS(index+1);
             }
-            level++;
         }
     }
 }
