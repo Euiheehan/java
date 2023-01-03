850ef7efe0d233a8ff2ed28c26a7d140844a668d
miniLove <han419120@naver.com>
2023-01-03 20:58:59 +0900

    더맵게(PriorityQueue)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,36 +1,26 @@
 package main;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Main().solution(new int[]{1, 1, 1, 1, 1}, 3));
+        System.out.println(new Main().solution(new int[]{1, 2, 3, 9, 10, 12}, 7));
     }
 
-    public int solution(int[] numbers, int target) {
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(0);
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> queue = Arrays.stream(scoville).boxed().collect(Collectors.toCollection(PriorityQueue::new));
 
         int count = 0;
-        int level = 0;
-        while(!queue.isEmpty()){
-            int len = queue.size();
-            for(int i = 0; i<len; i++){
-                Integer cur = queue.poll();
-                if(cur == null) { throw new NullPointerException();}
+        while(queue.element() < K){
+            Integer cur = queue.poll();
+            Integer min = queue.poll();
+            if(cur == null || min == null) return -1;
 
-                if(level == numbers.length){
-                    if(cur == target) count++;
-                }else{
-                    queue.offer(cur+numbers[level]);
-                    queue.offer(cur+(-1*numbers[level]));
-                }
-            }
-            level++;
-
+            queue.offer(cur + min*2);
+            count++;
         }
         return count;
     }
