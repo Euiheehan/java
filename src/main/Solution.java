9078fa2edfa6a741454f2d727a7c2e38293cb921
miniLove <han419120@naver.com>
2023-01-04 23:56:32 +0900

    숫자 문자열과 영단어 - 속도 향상, 코드 간결 버전

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class Main {
     public static void main(String[] args) {
 
@@ -10,33 +7,11 @@ class Main {
     }
 
     public int solution(String s) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("zero",0);
-        map.put("one",1);
-        map.put("two",2);
-        map.put("three",3);
-        map.put("four",4);
-        map.put("five",5);
-        map.put("six",6);
-        map.put("seven",7);
-        map.put("eight",8);
-        map.put("nine",9);
+        String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
 
-        StringBuilder result = new StringBuilder();
-        int start = 0;
-        for(int i = 1; i<=s.length(); i++){
-            String str = s.substring(start,i);
-
-            if(str.chars().allMatch(Character::isDigit)){
-                result.append(str);
-                start = i;
-            }
-
-            if(map.get(str) != null){
-                result.append(map.get(str));
-                start = i;
-            }
+        for(int i = 0; i<arr.length; i++){
+            s = s.replaceAll(arr[i], String.valueOf(i));
         }
-        return Integer.parseInt(result.toString());
+        return Integer.parseInt(s);
     }
 }
