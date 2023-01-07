7d5d02bbe7063c9cc10a0ffe7cbbf0453b1f3bf8
miniLove <han419120@naver.com>
2023-01-07 23:42:06 +0900

    실패율 - 이중포문 빼고 속도 향상 버전

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -11,19 +11,19 @@ class Main {
     public int[] solution(int N, int[] stages) {
         List<Integer> list = new ArrayList<>();
 
-        double totalPlayer = stages.length;
-
+        int totalPlayer = stages.length;
+        //0인덱스와 모두 클리어한 플레이어는 stage+1로 저장되기 때문에
+        //2가지 버전을 커버하기위해 +2를 해준다.
+        int[] notClear = new int[N+2];
+        for(int s : stages){
+            notClear[s]++;
+        }
         Map<Integer, Double> map = new HashMap<>();
 
         for(int i = 1; i <=N; i++){
-            double notClear = 0;
-            for(int s : stages){
-                if(i==s) notClear++;
-            }
-            map.put(i, totalPlayer == 0? 0 : notClear/totalPlayer);
             list.add(i);
-
-            totalPlayer = totalPlayer-notClear;
+            map.put(i, totalPlayer == 0? 0 : (double)notClear[i]/totalPlayer);
+            totalPlayer = totalPlayer-notClear[i];
         }
 
         list.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
