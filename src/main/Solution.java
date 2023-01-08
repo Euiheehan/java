6137b2fc52dbadba652636d1156b9fad00f5f5a1
miniLove <han419120@naver.com>
2023-01-08 23:25:04 +0900

    [1차]다트 게임 - stack버전 (속도는 이전이 빠름)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,42 +1,47 @@
 package main;
 
 
+import java.util.Stack;
+
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Main().solution("1S*2T*3S"));
+        System.out.println(new Main().solution("1D2S#10S"));
     }
 
     public int solution(String dartResult) {
 
-        int[] arr = new int[3];
-        int index = -1;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
         for(int i = 0; i<dartResult.length(); i++){
-            //숫자일 경우 인덱스 change
             if(Character.isDigit(dartResult.charAt(i))){
-                index++;
+                int now;
                 if(dartResult.charAt(i) == '1' && Character.isDigit(dartResult.charAt(i+1))){
-                    arr[index] = 10;
+                    now = 10;
                     //10일 경우는 다음 인덱스 skip
                     i++;
                 }else{
-                    arr[index] = Character.getNumericValue(dartResult.charAt(i));
+                    now = Character.getNumericValue(dartResult.charAt(i));
                 }
+                stack.push(now);
             }else{
+                int number = stack.pop();
                 switch (dartResult.charAt(i)) {
-                    case 'T' -> arr[index] = (int) Math.pow(arr[index], 3);
-                    case 'D' -> arr[index] = (int) Math.pow(arr[index], 2);
+                    case 'T' -> stack.push((int) Math.pow(number, 3));
+                    case 'D' -> stack.push((int) Math.pow(number, 2));
+                    //stack에 쌓으려면 1제곱도 필요함
+                    case 'S' -> stack.push((int) Math.pow(number, 1));
                     case '*' -> {
-                        arr[index] *= 2;
-                        if(index != 0) arr[index - 1] *= 2;
+                        stack.push(stack.pop()*2);
+                        stack.push(number*2);
                     }
-                    case '#' -> arr[index] *= (-1);
+                    case '#' -> stack.push(number*-1);
                 }
             }
         }
         int answer = 0;
-        for(int i : arr){
-            answer+=i;
+        while(!stack.isEmpty()){
+            answer+=stack.pop();
         }
         return answer;
     }
