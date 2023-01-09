b61822767eb70430d01a51b79e557363dc586382
miniLove <han419120@naver.com>
2023-01-09 21:37:33 +0900

    푸드 파이트 대회

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,33 +1,17 @@
 package main;
 
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(new Main().solution(new int[]{44, 1, 0, 0, 31, 25}, new int[]{31, 10, 45, 1, 6, 19})));
+        System.out.println(new Main().solution(new int[]{1, 3, 4, 6}));
     }
 
-    public int[] solution(int[] lottos, int[] win_nums) {
-        int[] arr = new int[]{6,6,5,4,3,2,1};
-
-        int removeNumbers = 0;
-        int matchNumbers = 0;
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int i : lottos){
-            if(i == 0) removeNumbers++;
-            else map.put(i, 1);
+    public String solution(int[] food) {
+        StringBuilder answer = new StringBuilder();
+        for(int i =1; i< food.length; i++){
+            answer.append(String.valueOf(i).repeat(food[i]/2));
         }
-
-        for(int num : win_nums){
-            if(map.get(num) != null) matchNumbers++;
-        }
-
-        return new int[]{arr[matchNumbers+removeNumbers], arr[matchNumbers]};
+        StringBuilder reverseAnswer = new StringBuilder(answer);
+        return answer.append(0).append(reverseAnswer.reverse()).toString();
     }
 }
