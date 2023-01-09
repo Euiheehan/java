f65cea1ec42871ad13f09c565207f4409be55287
miniLove <han419120@naver.com>
2023-01-09 20:22:42 +0900

    로또의 최고 순위와 최저 순위

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,48 +1,25 @@
 package main;
 
 
-import java.util.Stack;
+import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Main().solution("1D2S#10S"));
+        System.out.println(Arrays.toString(new Main().solution(new int[]{44, 1, 0, 0, 31, 25}, new int[]{31, 10, 45, 1, 6, 19})));
     }
 
-    public int solution(String dartResult) {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] arr = new int[]{6,6,5,4,3,2,1};
 
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        for(int i = 0; i<dartResult.length(); i++){
-            if(Character.isDigit(dartResult.charAt(i))){
-                int now;
-                if(dartResult.charAt(i) == '1' && Character.isDigit(dartResult.charAt(i+1))){
-                    now = 10;
-                    //10일 경우는 다음 인덱스 skip
-                    i++;
-                }else{
-                    now = Character.getNumericValue(dartResult.charAt(i));
-                }
-                stack.push(now);
-            }else{
-                int number = stack.pop();
-                switch (dartResult.charAt(i)) {
-                    case 'T' -> stack.push((int) Math.pow(number, 3));
-                    case 'D' -> stack.push((int) Math.pow(number, 2));
-                    //stack에 쌓으려면 1제곱도 필요함
-                    case 'S' -> stack.push((int) Math.pow(number, 1));
-                    case '*' -> {
-                        stack.push(stack.pop()*2);
-                        stack.push(number*2);
-                    }
-                    case '#' -> stack.push(number*-1);
-                }
-            }
+        int removeNumbers = 0;
+        int matchNumbers = 0;
+
+        for(int i : lottos){
+            if(i == 0) removeNumbers++;
+            else if(Arrays.stream(win_nums).anyMatch(value -> value == i)) matchNumbers++;
         }
-        int answer = 0;
-        while(!stack.isEmpty()){
-            answer+=stack.pop();
-        }
-        return answer;
+
+        return new int[]{arr[matchNumbers+removeNumbers], arr[matchNumbers]};
     }
 }
