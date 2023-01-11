4904c95effbbf4e291c104be03e040c2bc341ce3
miniLove <han419120@naver.com>
2023-01-11 21:12:04 +0900

    과일 장수

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,24 +1,27 @@
 package main;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(new Main().solution("banana")));
+        System.out.println(new Main().solution(3,4, new int[]{1, 2, 3, 1, 2, 3, 1}));
     }
 
-    public int[] solution(String s) {
-        int[] answer = new int[s.length()];
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
 
-        Map<Character, Integer> map = new HashMap<>();
+        if(score.length < m) return 0;
 
-        for(int i = 0; i<s.length();i++){
-            answer[i] = map.containsKey(s.charAt(i))? i-map.get(s.charAt(i)) : -1;
-            map.put(s.charAt(i), i);
+        int count = 1;
+        for(int i = score.length-1; i >= 0; i--){
+            if(count%m == 0){
+                answer += score[i]*m;
+            }
+            count++;
         }
+
         return answer;
     }
 }
