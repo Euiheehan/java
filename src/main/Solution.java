fa7d97b4e393e1309b5530dfcc9fb4b1e90286c0
miniLove <han419120@naver.com>
2023-01-13 23:49:00 +0900

    크레인 인형뽑기 게임

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,22 +1,33 @@
 package main;
 
+import java.util.Stack;
+
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Main().solution(10));
+        System.out.println(new Main().solution(new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}}, new int[]{1,5,3,5,1,2,1,4}));
     }
 
-    public int solution(int n) {
-        int answer = 0;
-        int[] arr = new int[n+1];
-        for(int i= 2; i<= n; i++){
-            if(arr[i] == 0){
-                answer++;
-                for(int j = i; j<=n; j+=i){
-                    arr[j] = 1;
+    public int solution(int[][] board, int[] moves) {
+        Stack<Integer> stack = new Stack<>();
+
+        int count = 0;
+        for(int m : moves){
+            int realM = m-1;
+            for(int i =0; i<board.length; i++){
+                if(board[i][realM] != 0){
+                    if(stack.size( ) > 0 && stack.peek() == board[i][realM]){
+                        count+=2;
+                        stack.pop();
+                    }else{
+                        stack.push(board[i][realM]);
+                    }
+                    board[i][realM] = 0;
+                    break; // 그 아래꺼까지 가져오기 때문에 break 해줘야함
                 }
             }
         }
-        return answer;
+
+        return count;
     }
 }
