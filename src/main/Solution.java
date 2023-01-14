e8feb87a6782931b0fea5dde66f9ed32c7d0cf9c
miniLove <han419120@naver.com>
2023-01-14 16:44:10 +0900

    햄버거 만들기 - for문 줄였으나 속도는 더 오래걸림

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -5,37 +5,28 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Main().solution(new int[]{1, 3, 2, 1, 2, 1, 3, 1, 2}));
+        System.out.println(new Main().solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1}));
     }
 
     public int solution(int[] ingredient) {
-
-        Stack<Integer> stack1 = new Stack<>();
-        List<Integer> list = new ArrayList<>();
-
-        int count = 0;
         int answer = 0;
-        int[] arr = new int[]{1,3,2,1};
+
+        List<Integer> list = new ArrayList<>();
+        int index = 0;
 
         for(int i : ingredient){
-            stack1.push(i);
-            //햄버거를 만들 수 있는 최소 수량 이상, 현재 포인터가 1 == 빵 인 경우만 check
-            if(stack1.size() > 3 && stack1.peek() == 1){
-                for (int k : arr) {
-                    int pop = stack1.pop();
-                    //햄버거 순서가 아닐 경우를 대비해
-                    list.add(pop);
-                    //햄버거 순서가 아닐 경우 이전까지 뺀 stack 원소 순서대로 넣기
-                    if (k != pop) {
-                        Collections.reverse(list);
-                        stack1.addAll(list);
-                        break;
-                    }
-                    count++;
-                }
-                list.clear();
-                if(count == 4) answer++;
-                count = 0;
+            list.add(index, i);
+
+            if(index >= 3 && list.get(index) == 1
+                && list.get(index-1) == 3
+                && list.get(index-2) == 2
+                && list.get(index-3) == 1
+            ){
+                answer++;
+                index-=3;
+
+            }else{
+              index++;
             }
         }
         return answer;
