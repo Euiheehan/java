ac057bc21e6747cb8761afca814e0547fbeffbc3
miniLove <han419120@naver.com>
2023-01-16 21:47:15 +0900

    숫자 짝꿍

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,27 +1,31 @@
 package main;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(new Main().solution(3, new int[]{10, 100, 20, 150, 1, 100, 200})));
+        System.out.println(new Main().solution("5525","1255"));
     }
 
-    public int[] solution(int k, int[] score) {
-        int[] answer = new int[score.length];
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(k);
+    public String solution(String X, String Y) {
+        List<Character> list = new ArrayList<>();
 
-        for(int i=0; i<score.length; i++){
-            priorityQueue.offer(score[i]);
-            //사이즈가 k번 넘어가면 제일 작은 수 제거하고
-            if(priorityQueue.size() > k) {
-                priorityQueue.poll();
-            }
-            //제거한 큐에서 제일 작은 숫자
-            answer[i] = priorityQueue.element();
+        Map<Character, Integer> map = new HashMap<>();
+        for(char y : Y.toCharArray()){
+            map.put(y, map.getOrDefault(y,0)+1);
         }
-        return answer;
+
+        for(char x : X.toCharArray()){
+           if(map.getOrDefault(x,0) !=0){
+               map.put(x, map.get(x)-1);
+               list.add(x);
+           }
+        }
+
+        if(list.isEmpty()) return "-1";
+        if(list.stream().allMatch(character -> character == '0')) return "0";
+        return list.stream().sorted(Comparator.reverseOrder()).map(String::valueOf).collect(Collectors.joining());
     }
 }
