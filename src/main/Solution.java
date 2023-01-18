6c4b3e8d65671c87ae7a66a4a30bf42aabf770f1
miniLove <han419120@naver.com>
2023-01-18 23:41:27 +0900

    LRU-캐시메모리

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,29 +1,40 @@
 package main;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 class Main {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        String s = in.nextLine();
+        int N = in.nextInt();
+        int M = in.nextInt();
 
-        System.out.print(new Main().solution(s));
+        int[] arr= new int[M];
+        for(int i =0; i<M; i++){
+            arr[i] = in.nextInt();
+        }
+        int[] result = new Main().solution(N,arr);
+        for(int i : result){
+            System.out.print(i+" ");
+        }
+
     }
 
-    public int solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        int sum = 0;
-        for (int i=0; i<s.length(); i++){
-            if(s.charAt(i) =='(') stack.push('(');
-            else {
-                stack.pop();
-                //레이저용 닫는 괄호면
-                if(s.charAt(i-1) == '(') sum += stack.size();
-                    //선분용 닫는 괄호면
-                else sum++;
+
+    public int[] solution(int n, int[] arr) {
+
+        int[] result = new int[n];
+
+        for(int num : arr){
+            int pos = n-1; //인덱스 번호, 위치
+            for(int i=0; i<n; i++){
+                if(num == result[i]) pos = i; //같은 값이 있으면 같은 값이 있는 인덱스 넣어주고, 아니면 -1
             }
+
+            //같은 값 없을 때(pos = n-1),끝->앞으로 가면서 하나씩 뒤로 땡기기
+            //같은 값 있을 때 (pos =i), 같은 지점 -> 앞으로 가면서 하나씩 뒤로 땡기기
+            if (pos >= 0) System.arraycopy(result, 0, result, 1, pos);
+            result[0] = num;
         }
-        return sum;
+        return result;
     }
 }
