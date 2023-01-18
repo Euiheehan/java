e4287a7b9f9c65cfbc57d0e806305bfdd304f6db
miniLove <han419120@naver.com>
2023-01-19 00:53:29 +0900

    옹알이(2)

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,40 +1,36 @@
 package main;
 
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int N = in.nextInt();
-        int M = in.nextInt();
-
-        int[] arr= new int[M];
-        for(int i =0; i<M; i++){
-            arr[i] = in.nextInt();
-        }
-        int[] result = new Main().solution(N,arr);
-        for(int i : result){
-            System.out.print(i+" ");
-        }
+      System.out.print(new Main().solution(new String[]{"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"}));
 
     }
+    public int solution(String[] babbling) {
+        int answer = 0;
+
+        String[] arr = new String[]{"aya", "ye", "woo", "ma"};
 
 
-    public int[] solution(int n, int[] arr) {
-
-        int[] result = new int[n];
-
-        for(int num : arr){
-            int pos = n-1; //인덱스 번호, 위치
-            for(int i=0; i<n; i++){
-                if(num == result[i]) pos = i; //같은 값이 있으면 같은 값이 있는 인덱스 넣어주고, 아니면 -1
+        for(String s : babbling){
+            for (int i = 0; i<arr.length; i++){
+                if(s.contains(arr[i])){
+                    s = s.replaceAll(arr[i], String.valueOf(i));
+                }
             }
 
-            //같은 값 없을 때(pos = n-1),끝->앞으로 가면서 하나씩 뒤로 땡기기
-            //같은 값 있을 때 (pos =i), 같은 지점 -> 앞으로 가면서 하나씩 뒤로 땡기기
-            if (pos >= 0) System.arraycopy(result, 0, result, 1, pos);
-            result[0] = num;
+            boolean isTrue = true;
+            if(s.chars().allMatch(Character::isDigit)) {
+                for(int i = 0; i<s.length()-1; i++){
+                    if(s.charAt(i)==s.charAt(i+1)){
+                        isTrue = false;
+                        break;
+                    }
+                }
+                if(isTrue){
+                    answer++;
+                }
+            }
         }
-        return result;
+        return answer;
     }
 }
