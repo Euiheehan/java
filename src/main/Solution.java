8c9f009b797ab347752f3b37c1a25d7fe5f076a1
miniLove <han419120@naver.com>
2023-01-23 06:54:03 +0900

    다리를 지나는 트럭 - 객체로 바꾸고 속도 향상 버전

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -4,47 +4,64 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-      System.out.print(new Main().solution(5, 5, new int[]{2,2,2,2,1,1,1,1,1}));
+      System.out.print(new Main().solution(2, 10, new int[]{7, 4, 5, 6}));
 
+    }
+    static class Truck{
+        int weight;
+        int move;
+
+        public Truck(int weight){
+            this.weight = weight;
+            this.move = 1;
+        }
+
+        public void moving(){
+            move++;
+        }
     }
     public int solution(int bridge_length, int weight, int[] truck_weights) {
 
         //대기트럭 Queue
-        Queue<Integer> truckQueue = new LinkedList<>();
+        Queue<Truck> truckQueue = new LinkedList<>();
         for(int truck : truck_weights){
-            truckQueue.offer(truck);
+            truckQueue.offer(new Truck(truck));
         }
 
         //다리 Queue
-        Queue<Integer> bridgeQueue = new LinkedList<>();
+        Queue<Truck> bridgeQueue = new LinkedList<>();
 
-        //첫번째 트럭
-        Integer o = truckQueue.poll();
-        int answer = 1;
-        int queueSum = o == null? 0: o;
-        bridgeQueue.offer(o);
+        int answer = 0;
+        int queueSum = 0;
 
-        //다리에 트럭이 하나도 없을 때까지(==모두 공백일 때)
-        while(!bridgeQueue.stream().allMatch(integer -> integer.equals(0))) {
-            //다음 트럭(꺼내지말고 하중만)
-            Integer cur = truckQueue.peek();
+        while(!bridgeQueue.isEmpty() || !truckQueue.isEmpty()){
+            answer++;
 
-            if(answer >= bridge_length){
-                Integer clear = bridgeQueue.poll();
-                queueSum -= clear == null? 0 : clear;
+            //첫번째 트럭
+            if (bridgeQueue.isEmpty()) {
+                Truck t = truckQueue.poll();
+                queueSum += t.weight;
+                bridgeQueue.offer(t);
+                continue;
+            }
+
+            //다리 위의 트럭 모두 한칸씩 움직이기
+            for(Truck t : bridgeQueue){
+                t.moving();
+            }
+            //다리위 트럭의 움직임이 다리길이보다 크면
+            if(!bridgeQueue.isEmpty() && bridgeQueue.peek().move > bridge_length){
+                Truck clear = bridgeQueue.poll();
+                queueSum -= clear == null? 0 : clear.weight;
             }
 
             //다리 최대 수량보다 미만이지만, 다음 트럭 싣게되면 무게는 미만일때
-            if(bridgeQueue.size() < bridge_length && cur != null && queueSum + cur <= weight){
-                Integer t = truckQueue.poll();
+            if(!truckQueue.isEmpty()
+                    && truckQueue.peek() != null && queueSum + truckQueue.peek().weight <= weight){
+                Truck t = truckQueue.poll();
+                queueSum += t == null? 0 : t.weight;
                 bridgeQueue.offer(t);
-                queueSum += t == null? 0 : t;
-            }else{
-                //아니면 공백 넣기
-                bridgeQueue.offer(0);
             }
-
-            answer++;
         }
         return answer;
     }
