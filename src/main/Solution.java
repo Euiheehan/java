5845c690a7522c47ddcf1dadb25d35b65a58d90d
miniLove <han419120@naver.com>
2023-01-24 15:56:22 +0900

    신규 아이디 추천 - 정규식으로 if문 줄여서 코드 간결, 속도 약간 향상 버전

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -7,20 +7,20 @@ class Main {
     }
 
     public String solution(String new_id) {
-        new_id = new_id.toLowerCase().replaceAll("[^-._0-9a-z]", "");
-        while(new_id.contains("..")){
-            new_id = new_id.replaceAll("\\.\\.", ".");
-        }
-        if(new_id.startsWith(".")) new_id = new_id.substring(1);
-        if(new_id.endsWith(".")) new_id = new_id.substring(0, new_id.length()-1);
+        new_id = new_id.toLowerCase()
+                .replaceAll("[^-._0-9a-z]", "")
+                .replaceAll("[.]{2,}", ".")
+                .replaceAll("^[.]|[.]$", "");
+
         if("".equalsIgnoreCase(new_id)) new_id = "a";
         if(new_id.length() > 15) new_id = new_id.substring(0,15);
-        if(new_id.endsWith(".")) new_id = new_id.substring(0, new_id.length()-1);
 
+        new_id = new_id.replaceAll("^[.]|[.]$", "");
+
+        StringBuilder sb = new StringBuilder(new_id);
         if(new_id.length() < 3) {
-            StringBuilder sb = new StringBuilder(new_id);
-            new_id = sb.append(String.valueOf(new_id.charAt(new_id.length() - 1)).repeat(3 - new_id.length())).toString();
+            sb.append(String.valueOf(new_id.charAt(new_id.length() - 1)).repeat(3 - new_id.length()));
         }
-        return new_id;
+        return sb.toString();
     }
 }
