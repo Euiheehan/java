cd2b8b0d5cd306b85ce055251ad5e59bfb3c3149
miniLove <han419120@naver.com>
2023-01-24 15:42:27 +0900

    신규 아이디 추천

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,68 +1,26 @@
 package main;
 
-import java.util.*;
-
 class Main {
     public static void main(String[] args) {
-      System.out.print(new Main().solution(2, 10, new int[]{7, 4, 5, 6}));
 
+        System.out.println(new Main().solution("...!@BaT#*..y.abcdefghijklm"));
     }
-    static class Truck{
-        int weight;
-        int move;
 
-        public Truck(int weight){
-            this.weight = weight;
-            this.move = 1;
+    public String solution(String new_id) {
+        new_id = new_id.toLowerCase().replaceAll("[^-._0-9a-z]", "");
+        while(new_id.contains("..")){
+            new_id = new_id.replaceAll("\\.\\.", ".");
         }
+        if(new_id.startsWith(".")) new_id = new_id.substring(1);
+        if(new_id.endsWith(".")) new_id = new_id.substring(0, new_id.length()-1);
+        if("".equalsIgnoreCase(new_id)) new_id = "a";
+        if(new_id.length() > 15) new_id = new_id.substring(0,15);
+        if(new_id.endsWith(".")) new_id = new_id.substring(0, new_id.length()-1);
 
-        public void moving(){
-            move++;
+        if(new_id.length() < 3) {
+            StringBuilder sb = new StringBuilder(new_id);
+            new_id = sb.append(String.valueOf(new_id.charAt(new_id.length() - 1)).repeat(3 - new_id.length())).toString();
         }
-    }
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-
-        //대기트럭 Queue
-        Queue<Truck> truckQueue = new LinkedList<>();
-        for(int truck : truck_weights){
-            truckQueue.offer(new Truck(truck));
-        }
-
-        //다리 Queue
-        Queue<Truck> bridgeQueue = new LinkedList<>();
-
-        int answer = 0;
-        int queueSum = 0;
-
-        while(!bridgeQueue.isEmpty() || !truckQueue.isEmpty()){
-            answer++;
-
-            //첫번째 트럭
-            if (bridgeQueue.isEmpty()) {
-                Truck t = truckQueue.poll();
-                queueSum += t.weight;
-                bridgeQueue.offer(t);
-                continue;
-            }
-
-            //다리 위의 트럭 모두 한칸씩 움직이기
-            for(Truck t : bridgeQueue){
-                t.moving();
-            }
-            //다리위 트럭의 움직임이 다리길이보다 크면
-            if(!bridgeQueue.isEmpty() && bridgeQueue.peek().move > bridge_length){
-                Truck clear = bridgeQueue.poll();
-                queueSum -= clear == null? 0 : clear.weight;
-            }
-
-            //다리 최대 수량보다 미만이지만, 다음 트럭 싣게되면 무게는 미만일때
-            if(!truckQueue.isEmpty()
-                    && truckQueue.peek() != null && queueSum + truckQueue.peek().weight <= weight){
-                Truck t = truckQueue.poll();
-                queueSum += t == null? 0 : t.weight;
-                bridgeQueue.offer(t);
-            }
-        }
-        return answer;
+        return new_id;
     }
 }
