134df0f2be92292d56b2e35cd2748476215758c8
miniLove <han419120@naver.com>
2023-01-24 16:16:21 +0900

    신규 아이디 추천 - 객체지향적인 버전

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -6,21 +6,68 @@ class Main {
         System.out.println(new Main().solution("...!@BaT#*..y.abcdefghijklm"));
     }
 
-    public String solution(String new_id) {
-        new_id = new_id.toLowerCase()
-                .replaceAll("[^-._0-9a-z]", "")
-                .replaceAll("[.]{2,}", ".")
-                .replaceAll("^[.]|[.]$", "");
+    static class KAKAO2021 {
 
-        if("".equalsIgnoreCase(new_id)) new_id = "a";
-        if(new_id.length() > 15) new_id = new_id.substring(0,15);
+        String s;
 
-        new_id = new_id.replaceAll("^[.]|[.]$", "");
-
-        StringBuilder sb = new StringBuilder(new_id);
-        if(new_id.length() < 3) {
-            sb.append(String.valueOf(new_id.charAt(new_id.length() - 1)).repeat(3 - new_id.length()));
+        KAKAO2021(String s){
+            this.s = s;
         }
-        return sb.toString();
+        public KAKAO2021 doLowerCase(){
+            this.s = s.toLowerCase();
+            return this;
+        }
+
+        public KAKAO2021 filter(){
+            this.s = s.replaceAll("[^-._0-9a-z]", "");
+            return this;
+        }
+
+        public KAKAO2021 noDots(){
+            this.s = s.replaceAll("[.]{2,}", ".");
+            return this;
+        }
+
+        public KAKAO2021 noStartDotOrEndDot(){
+            this.s = s.replaceAll("^[.]|[.]$", "");
+            return this;
+        }
+
+        public KAKAO2021 notEmpty(){
+            this.s = s.equalsIgnoreCase("")? "a" : s;
+            return this;
+        }
+
+        public KAKAO2021 notLongLength(){
+            this.s = s.length() > 15? s.substring(0,15) : s;
+            return this;
+        }
+
+        public KAKAO2021 limitLength(){
+            StringBuilder sb = new StringBuilder(s);
+            if(s.length() < 3) {
+               this.s = sb.append(String.valueOf(s.charAt(s.length() - 1)).repeat(3 - s.length())).toString();
+            }
+            return this;
+        }
+
+        public String getResult(){
+            return s;
+        }
+    }
+
+    public String solution(String new_id) {
+
+        KAKAO2021 id = new KAKAO2021(new_id);
+
+        return id.doLowerCase()
+                .filter()
+                .noDots()
+                .noStartDotOrEndDot()
+                .notEmpty()
+                .notLongLength()
+                .noStartDotOrEndDot()
+                .limitLength()
+                .getResult();
     }
 }
