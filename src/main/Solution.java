fe686b9bedaf4bbe6eb0f83214111d6117be9e70
miniLove <han419120@naver.com>
2023-02-03 22:25:22 +0900

    JadenCase 문자열 만들기

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -3,71 +3,20 @@ package main;
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Main().solution("...!@BaT#*..y.abcdefghijklm"));
+        System.out.println(new Main().solution(" 3  aREWF  Dffdd   fdfddr "));
     }
 
-    static class KAKAO2021 {
-
-        String s;
-
-        KAKAO2021(String s){
-            this.s = s;
-        }
-        public KAKAO2021 doLowerCase(){
-            this.s = s.toLowerCase();
-            return this;
-        }
-
-        public KAKAO2021 filter(){
-            this.s = s.replaceAll("[^-._0-9a-z]", "");
-            return this;
-        }
-
-        public KAKAO2021 noDots(){
-            this.s = s.replaceAll("[.]{2,}", ".");
-            return this;
-        }
-
-        public KAKAO2021 noStartDotOrEndDot(){
-            this.s = s.replaceAll("^[.]|[.]$", "");
-            return this;
-        }
-
-        public KAKAO2021 notEmpty(){
-            this.s = s.equalsIgnoreCase("")? "a" : s;
-            return this;
-        }
-
-        public KAKAO2021 notLongLength(){
-            this.s = s.length() > 15? s.substring(0,15) : s;
-            return this;
-        }
-
-        public KAKAO2021 limitLength(){
-            StringBuilder sb = new StringBuilder(s);
-            if(s.length() < 3) {
-               this.s = sb.append(String.valueOf(s.charAt(s.length() - 1)).repeat(3 - s.length())).toString();
+    public String solution(String s) {
+        s = s.toLowerCase();
+        String[] arr = s.split(" ");
+        StringBuilder builder = new StringBuilder();
+        for(String a : arr){
+            //공백문자가 연속해서 나올 수 있습니다.
+            if(!a.isEmpty()){
+                builder.append(a.substring(0,1).toUpperCase()).append(a.substring(1));
             }
-            return this;
+            builder.append(" ");
         }
-
-        public String getResult(){
-            return s;
-        }
-    }
-
-    public String solution(String new_id) {
-
-        KAKAO2021 id = new KAKAO2021(new_id);
-
-        return id.doLowerCase()
-                .filter()
-                .noDots()
-                .noStartDotOrEndDot()
-                .notEmpty()
-                .notLongLength()
-                .noStartDotOrEndDot()
-                .limitLength()
-                .getResult();
+        return s.endsWith(" ")?builder.toString() : builder.toString().trim();
     }
 }
