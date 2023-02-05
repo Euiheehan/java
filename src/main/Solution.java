392c93059512fee779f20588b6fc1469635ea357
miniLove <han419120@naver.com>
2023-02-05 21:40:25 +0900

    최솟값 만들기

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,21 +1,22 @@
 package main;
 
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Main().solution(" 3  aREWF  Dffdd   fdfddr "));
+        System.out.println(new Main().solution(new int[]{1, 4, 2}, new int[]{5,4,4}));
     }
 
-    public String solution(String s) {
-        s = s.toLowerCase();
-        StringBuilder sb = new StringBuilder();
-        boolean isStartWith = true;
+    public int solution(int []A, int []B) {
+        int sum = 0;
 
-        for(char c : s.toCharArray()){
-            sb.append(isStartWith? Character.toUpperCase(c) : c);
-            isStartWith = c == ' ';
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i = 0; i<A.length; i++){
+            sum += A[i] * B[B.length-1-i];
         }
-
-        return sb.toString();
+        return sum;
     }
 }
