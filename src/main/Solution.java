6fe87fb5142aa7752675bf8832b343e3fc11e9d6
miniLove <han419120@naver.com>
2023-02-09 23:15:34 +0900

    숫자의 표현

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,22 +1,21 @@
 package main;
 
-import java.util.Arrays;
-
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Main().solution(new int[]{1, 4, 2}, new int[]{5,4,4}));
+        System.out.println(new Main().solution(15));
     }
 
-    public int solution(int []A, int []B) {
-        int sum = 0;
-
-        Arrays.sort(A);
-        Arrays.sort(B);
-
-        for(int i = 0; i<A.length; i++){
-            sum += A[i] * B[B.length-1-i];
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i<=n; i++){
+            int sum = 0;
+            for(int k =i; k<=n; k++){
+                sum+=k;
+                if(sum==n) answer++;
+                if(sum>n) break;
+            }
         }
-        return sum;
+        return answer;
     }
 }
