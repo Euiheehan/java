d07c531b18c55a392cd4a3161eeb3d57d63dc2f9
euiheeHan <han419120@naver.com>
2024-02-18 22:53:09 +0900

    카드뭉치

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,21 +1,35 @@
 package main;
 
+import java.util.Arrays;
+
 class Main {
     public static void main(String[] args) {
 
-        System.out.println(new Main().solution(15));
+        System.out.println(new Main().solution(
+
+            new String[] {"i", "water", "drink"},
+            new String[] {"want", "to"},
+            new String[] {"i", "want", "to", "drink", "water"}
+        ));
     }
 
-    public int solution(int n) {
-        int answer = 0;
-        for(int i = 1; i<=n; i++){
-            int sum = 0;
-            for(int k =i; k<=n; k++){
-                sum+=k;
-                if(sum==n) answer++;
-                if(sum>n) break;
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+
+        int cards1Num = 0;
+        int cards2Num = 0;
+
+        for(String s : goal) {
+            int findCard1Index = Arrays.asList(cards1).indexOf(s) + 1;
+            int findCard2Index = Arrays.asList(cards2).indexOf(s) + 1;
+
+            if (findCard1Index != 0 && findCard1Index - cards1Num == 1) {
+                cards1Num++;
+            }else if(findCard2Index != 0 && findCard2Index - cards2Num == 1) {
+                cards2Num++;
+            }else{
+                return "No";
             }
         }
-        return answer;
+        return "Yes";
     }
 }
