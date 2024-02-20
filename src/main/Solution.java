ac9fdb3aa9165deb0fb1d9695e3c69946a715cdd
euiheeHan <han419120@naver.com>
2024-02-20 23:36:12 +0900

    다음 큰 숫자

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,35 +1,17 @@
 package main;
 
-import java.util.Arrays;
+import static java.lang.Long.bitCount;
 
 class Main {
-    public static void main(String[] args) {
 
-        System.out.println(new Main().solution(
+  public static void main(String[] args) {
 
-            new String[] {"i", "water", "drink"},
-            new String[] {"want", "to"},
-            new String[] {"i", "want", "to", "drink", "water"}
-        ));
-    }
+    System.out.println(new Main().solution(78));
+  }
 
-    public String solution(String[] cards1, String[] cards2, String[] goal) {
-
-        int cards1Num = 0;
-        int cards2Num = 0;
-
-        for(String s : goal) {
-            int findCard1Index = Arrays.asList(cards1).indexOf(s) + 1;
-            int findCard2Index = Arrays.asList(cards2).indexOf(s) + 1;
-
-            if (findCard1Index != 0 && findCard1Index - cards1Num == 1) {
-                cards1Num++;
-            }else if(findCard2Index != 0 && findCard2Index - cards2Num == 1) {
-                cards2Num++;
-            }else{
-                return "No";
-            }
-        }
-        return "Yes";
+    public int solution(int n) {
+        int bitCount = bitCount(n);
+        while (bitCount != bitCount(++n)) {}
+        return n;
     }
 }
